941e46f27a619dd66f9e8cd8e75369225a77a562
nvtung0604 <ninhviettung04@gmail.com>
2025-06-05 16:24:59 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// () => {}
+
+const MyComponent = () => {
+  return (<div>Tung hoc react library</div>);
+};
+
+
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -17,6 +22,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello React</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
