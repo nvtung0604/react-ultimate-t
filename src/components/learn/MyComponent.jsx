2d7c7951df8771a7b2740a1caa637e6d8d931f68
nvtung0604 <ninhviettung04@gmail.com>
2025-06-05 23:39:27 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return (<div>Tung hoc react library</div>);
+};
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,14 @@
+const ThirdComponent = () => {
+    return (
+        <div>Third Component</div>
+    );
+}
+
+
+const SecondComponent = () => {
+    return (
+        <div>Second component</div>
+    );
+}
+
+export {SecondComponent, ThirdComponent};
