0a35e2de3756aea4f00a373f2d9f8a44d408af17
nvtung0604 <ninhviettung04@gmail.com>
2025-06-06 10:11:07 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,22 @@
 import './style.css'
 
 const MyComponent = () => {
+  // const hoidanit = "tung";
+  // const num = 25;
+  const tung = {
+    name: "tung",
+    age: "21"
+  }
   return (
     // Fragment
     <>
-    <div className="">Tung hoc react library</div>
-    <div className="child">Fragment</div>
+    <div className=""> {JSON.stringify(tung.age)} Tung hoc react library</div>
+    {/*
+      dấu nháy đôi đầu tiên tượng trưng cho style viết trong html,
+      dấu nháy đôi hai tượng trưng cho object
+     */}
+     <div>{console.log("Tung learn React")}</div>
+    <div className="child" style = {{borderRadius: "10px"}}>Fragment</div>
     </>
   );
 };
