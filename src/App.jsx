71824ab792e60bbdbea7edbab95217d4579b3564
nvtung0604 <ninhviettung04@gmail.com>
2025-06-15 19:48:16 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,43 @@
-import './components/todo/todo.css';
-import TodoData from './components/todo/TodoData';
-import TodoNew from './components/todo/TodoNew';
-import reactLogo from './assets/react.svg';
-import { useState } from 'react';
-import Header from './components/layouts/header';
-import Footer from './components/layouts/footer';
-
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
-  const [todoList, setTodoList] = useState([
-    
-  ])
-  const addNewtodo = (name) => {
-    const newTodo = {id: randomIntFromInterval(1, 10000000), name: name}
-    setTodoList([...todoList, newTodo])
-  };
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  const del = (id) => {
-     setTodoList(todoList.filter(item=> item.id !== id))
-  }
-  return (
-    <>
-    <Header/>
-    <div className="todo-app">
-      <div className="todo-app__inner">
-        <h2 className="todo-app__title">
-          To do list
-        </h2>
-        <TodoNew
-          addNewtodo = {addNewtodo}
-        />
-        {todoList.length > 0 ?
-        <TodoData
-          del = {del}
-          todoList = {todoList}
-          
-        />
-         :
-        <div className="todo-app__img-wrap">
-          
-          <img src={reactLogo} alt="" className="logo" />
-        </div>
-        }
-      </div>
-    </div>
-    <Footer/>
-    </>
-  );
-}
+    const [todoList, setTodoList] = useState([]);
+    const addNewtodo = (name) => {
+        const newTodo = { id: randomIntFromInterval(1, 10000000), name: name };
+        setTodoList([...todoList, newTodo]);
+    };
+    const randomIntFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const del = (id) => {
+        setTodoList(todoList.filter((item) => item.id !== id));
+    };
+    return (
+        <>
+            <Header />
+            <div className="todo-app">
+                <div className="todo-app__inner">
+                    <h2 className="todo-app__title">To do list</h2>
+                    <TodoNew addNewtodo={addNewtodo} />
+                    {todoList.length > 0 ? (
+                        <TodoData del={del} todoList={todoList} />
+                    ) : (
+                        <div className="todo-app__img-wrap">
+                            <img src={reactLogo} alt="" className="logo" />
+                        </div>
+                    )}
+                </div>
+            </div>
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import LoginPage from './pages/login.jsx';
-import RegisterPage from './pages/register.jsx';
-import UsersPage from './pages/users.jsx';
-import ProductsPage from './pages/products.jsx';
-import './style/global.css'
-const router = createBrowserRouter([ 
-{ 
-path: "/", 
-element: <App/>, 
-},
-{
-  path: "/login",
-  element: <LoginPage/>
-} ,
-{
-  path: "/register",
-  element: <RegisterPage/>
-},
-{
-  path: "/users",
-  element: <UsersPage/>
-},
-{
-  path: "/products",
-  element: <ProductsPage/>
-} 
-]); 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UsersPage from "./pages/users.jsx";
+import ProductsPage from "./pages/products.jsx";
+import "./style/global.css";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/users",
+                element: <UsersPage />,
+            },
+            {
+                path: "/products",
+                element: <ProductsPage />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/register",
+        element: <RegisterPage />,
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        {/* <App /> */}
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
